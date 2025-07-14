2d4721c9e4df61f852201928970034b8c1ea1ce7
reshmaasha <40304137+reshmaasha@users.noreply.github.com>
2025-07-14 13:46:45 -0500

    Update streamlit_app.py
    adding df

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,5 +35,9 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered'+','+name_on_order+'!',icon="✅")
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df=st.dataframe(smoothiefroot_response.json(),use_container_width=True)
+if ingredients_list:
+  ingredients_string = '' 
+  for fruit_chosen in ingredients_list:
+    ingredients_string += fruit_chosen + ' '
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    sf_df=st.dataframe(smoothiefroot_response.json(),use_container_width=True)
